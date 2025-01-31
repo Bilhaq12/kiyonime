bf3c18f19a26182b52bdad2305622b2e0ab9841b
bilhaq12 <iboysgntg@gmail.com>
2025-01-31 18:08:04 +0000

    modified:   kiyocomics/components/hero.tsx

--- kiyocomics/components/hero.tsx
+++ kiyocomics/components/hero.tsx
@@ -7,7 +7,7 @@ export function Hero() {
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{
-          backgroundImage: `url('https://sjc.microlink.io/ik9AWtGiod1D1w4rJBr2lX60y2Ile7hnBNym8PnSBevC77wJ3gaku6-RQmQazFQfZXuxUDqCzHGOwh5NomOD2A.jpeg')`,
+          backgroundImage: `url('https://cdn.myanimelist.net/images/anime/1015/138006.jpg')`,
           backgroundPosition: "right center",
         }}
       >
@@ -18,10 +18,9 @@ export function Hero() {
           <span className="mr-2">1</span>
           <span>on Trend</span>
         </div>
-        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 max-w-2xl">Dr. STONE SCIENCE FUTURE</h1>
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 max-w-2xl">Sousou no Frieren</h1>
         <p className="text-muted-foreground max-w-2xl mb-8 text-sm sm:text-base">
-          The first cour of the fourth season of Dr.STONE. Senkuu and the Kingdom of Science revive Tsukasa and build a
-          spaceship to reach 'Why Man' on the Moon! (Source: Crunchyroll News, edited)
+        During their decade-long quest to defeat the Demon King, the members of the hero's party—Himmel himself, the priest Heiter, the dwarf warrior Eisen, and the elven mage Frieren—forge bonds through adventures and battles, creating unforgettable precious memories for most of them.! (Source: Crunchyroll News, edited)
         </p>
         <Button className="bg-purple-600 hover:bg-purple-700 w-full sm:w-auto">
           <Play className="mr-2 h-4 w-4" />
