e68f256f2d57f8169844ec1d19f8f6ad683c505e
bilhaq12 <iboysgntg@gmail.com>
2025-02-04 11:16:13 +0700

    Update page.tsx

--- kiyocomics/app/anime/[param]/page.tsx
+++ kiyocomics/app/anime/[param]/page.tsx
@@ -3,13 +3,13 @@ import { getEpisodeDetail } from "@/lib/api"
 import { EpisodeDetailClient } from "./episode-detail-client"
 import Loading from "./loading"
 
-interface PageProps {
-  params: {
-    param: string
-  }
+// Define the correct type for Next.js App Router page props
+type Props = {
+  params: { param: string }
+  searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export default async function EpisodeDetailPage({ params }: PageProps) {
+export default async function EpisodeDetailPage({ params }: Props) {
   const episodeDetail = await getEpisodeDetail(params.param)
 
   return (
@@ -17,4 +17,14 @@ export default async function EpisodeDetailPage({ params }: PageProps) {
       <EpisodeDetailClient initialEpisodeDetail={episodeDetail} />
     </Suspense>
   )
+}
+
+// Optionally, you can generate metadata for the page
+export async function generateMetadata({ params }: Props) {
+  const episodeDetail = await getEpisodeDetail(params.param)
+
+  return {
+    title: episodeDetail.title,
+    description: episodeDetail.synopsis,
+  }
 }
